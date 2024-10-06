413a486e27d52af9f2a593bb03e6120edd4b8c47
kousaatsu <GrigoriantsSM21@st.ithub.ru>
2024-10-06 20:02:29 +0300

    wip: dialogue system

---
+++ Blackie/Assets/_Source/DialogueSystem/DialogueBox.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+namespace DialogueSystem
+{
+    public class DialogueBox : MonoBehaviour
+    {
+        //[SerializeField] List<Sprite> _sprites;
+        [SerializeField] List<string> _names;
+        //[SerializeField] GameObject _imageObj;
+        [SerializeField] TextMeshProUGUI _name;
+        [SerializeField] TextMeshProUGUI _text;
+
+        private void Awake()
+        {
+            //List<Sprite> sprites = new List<Sprite>();
+            List<string> names = new List<string>();
+        }
+
+        public void Open()
+        {
+            Time.timeScale = 1.0f;
+            gameObject.SetActive(true);
+        }
+
+        public void Change(int _characterID, string _textOut)
+        {
+            //_imageObj.GetComponent<Image>().sprite = _sprites[_characterID];
+            _name.text = _names[_characterID];
+            _text.text = _textOut;
+        }
+
+        public void Close()
+        {
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
+        }
+    }
+
+}
